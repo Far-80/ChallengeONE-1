a8ea6dead6c2b8b01d864a880b1754023f517422
Fabian <farodriguez80@gmail.com>
2022-01-20 18:12:42 -0300

    codigo para encriptado de texto

--- js/form.js
+++ js/form.js
@@ -5,7 +5,7 @@ function dividirCadena(cadenaADividir) {
     var separador = " ";
     var arrayDeCadenas = cadenaADividir.split(separador);
     return arrayDeCadenas;
- }
+ };
 
 
 encriptar.addEventListener("click", function (){
@@ -24,14 +24,40 @@ encriptar.addEventListener("click", function (){
         while ( contador <= arrayPalabras.length){
             if(!patron.test(arrayPalabras[contador])){
                 alert ("el texto contiene caracteres invalidos");
+                break;
             }else{
-                console.log("seguir trabajando con el codigo");
+                validarTexto = true;
             }
             contador++;
 
         }
     }
+   
     
+    if (validarTexto) {
+        for (i=0; i < arrayPalabras.length; i++){
+            var palabra = arrayPalabras[i];
+            var texto = "";
+            console.log(palabra);
+            for (j=0; j < palabra.length; j++){
+                if (palabra[j] == "a"){
+                    texto = texto + "ai";
+                } else if (palabra[j] == "e"){
+                    texto = texto + "enter";
+                } else if (palabra[j] == "i"){
+                    texto = texto + "imes";
+                } else if (palabra[j] == "o"){
+                    texto = texto + "ober";
+                } else if (palabra[j] == "u"){
+                    texto = texto + "ufat";
+                }else{
+                    texto = texto + palabra[j];
+                }
+            }
+            console.log(texto);
+            arrayPalabras[i] = texto;
+            
+        }
+    }
 
 });
-
