e8f15f1500f23213d77eec261c76e50a86180f1d
Fabian <farodriguez80@gmail.com>
2022-01-21 01:40:21 -0300

    creacion de funcion validar texto y funcion encriptar

--- js/form.js
+++ js/form.js
@@ -7,57 +7,53 @@ function dividirCadena(cadenaADividir) {
     return arrayDeCadenas;
  };
 
+function validarTexto (texto){
+    const patron = new RegExp('^[a-z\s]');
+    var valido = false;
+
+    if (texto == "" || !patron.test(texto)){
+        alert ("Texto invalido: esta vacio, contiene mayuscula, contiene acento o es algún caracter!");
+    }
+    else {
+        valido = true;
+    }
+    return valido;
+}
+
+function encriptador (texto){
+    arrayPalabras = dividirCadena(texto);
+    for (i=0; i < arrayPalabras.length; i++){
+        var palabra = arrayPalabras[i];
+        var texto = "";
+        console.log(palabra);
+        for (j=0; j < palabra.length; j++){
+            if (palabra[j] == "a"){
+                texto = texto + "ai";
+            } else if (palabra[j] == "e"){
+                texto = texto + "enter";
+            } else if (palabra[j] == "i"){
+                texto = texto + "imes";
+            } else if (palabra[j] == "o"){
+                texto = texto + "ober";
+            } else if (palabra[j] == "u"){
+                texto = texto + "ufat";
+            }else{
+                texto = texto + palabra[j];
+            }
+        }
+        console.log(texto);
+        arrayPalabras[i] = texto;
+    }
+    return arrayPalabras;
+}
+
 
 encriptar.addEventListener("click", function (){
     var texto =document.querySelector("#id-texto-ingresado").value;
+    var textoProcesado = document.querySelector(".texto-procesado");
+    var arrayPalabras;
 
-    var validarTexto = false;
-    const patron = new RegExp('^[a-z]+$');
-
-    if (texto == ""){
-        alert ("Tiene que ingresar un texto")
+    if (validarTexto(texto)) {
+        encriptador(texto);
     }
-    else {
-        var arrayPalabras = dividirCadena (texto);
-        console.log(arrayPalabras);
-        var contador = 0;
-        while ( contador <= arrayPalabras.length){
-            if(!patron.test(arrayPalabras[contador])){
-                alert ("el texto contiene caracteres invalidos");
-                break;
-            }else{
-                validarTexto = true;
-            }
-            contador++;
-
-        }
-    }
-   
-    
-    if (validarTexto) {
-        for (i=0; i < arrayPalabras.length; i++){
-            var palabra = arrayPalabras[i];
-            var texto = "";
-            console.log(palabra);
-            for (j=0; j < palabra.length; j++){
-                if (palabra[j] == "a"){
-                    texto = texto + "ai";
-                } else if (palabra[j] == "e"){
-                    texto = texto + "enter";
-                } else if (palabra[j] == "i"){
-                    texto = texto + "imes";
-                } else if (palabra[j] == "o"){
-                    texto = texto + "ober";
-                } else if (palabra[j] == "u"){
-                    texto = texto + "ufat";
-                }else{
-                    texto = texto + palabra[j];
-                }
-            }
-            console.log(texto);
-            arrayPalabras[i] = texto;
-            
-        }
-    }
-
 });
